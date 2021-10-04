5e48cdb707752d165bff1454fa00f5b236b6f840
Julya Brustolin <julyab.marssona@gmail.com>
2021-10-04 15:26:09 -0300

    First commit

---
+++ pages/dashboard.tsx
@@ -0,0 +1,36 @@
+import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
+import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
+
+export default function Dashboard() {
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        api.get('/me').then(response => console.log(response)).catch(err => console.log(err))
+    }, [])
+
+    return (
+        <>
+        <h1>Bem vindo: {user?.email}</h1>
+
+        <Can permissions={['metrics.list']}>
+            <div>Métricas</div>
+       </Can>
+        </>
+    )
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get('/me');
+
+    return {
+        props:{
+
+        }
+    }
+})
